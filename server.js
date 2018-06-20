b51a30a1269b55473e872cd661fd8dfaa6d28847
tmarkes <thomas.markes@capgemini.com>
2018-06-20 08:37:43 -0700

    Switching in environment variable for external host where doable

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL_URLS);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
